d6184b6f29872c3f2165b89d58da339f57cbe366
nilson <nilson.education@outlook.com>
2024-06-04 17:05:25 -0500

    publishing docs folder

---
+++ docs/server/chunk-ZE65QIAS.mjs
@@ -0,0 +1,4 @@
+import './polyfills.server.mjs';
+import{A as g,a as i,b as p,c as n,d as m,e as r,f as a,m as f,o as s,p as d,q as c,t as l,x as u}from"./chunk-M7V42Q2Y.mjs";var v=(()=>{let t=class t{constructor(){this.title="app-example"}};t.\u0275fac=function(o){return new(o||t)},t.\u0275cmp=i({type:t,selectors:[["app-root"]],standalone:!0,features:[a],decls:4,vars:0,template:function(o,R){o&1&&(r(0,`Validating the free deployment!!
+New Update.
+`),p(1,"p"),r(2,"third Update"),n(),m(3,"router-outlet"))},dependencies:[s,u]});let e=t;return e})();var x=[];var C={providers:[g(x),c()]};var A={providers:[l()]},h=f(C,A);var M=()=>d(v,h),B=M;export{B as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZE65QIAS.mjs";import"./chunk-M7V42Q2Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
